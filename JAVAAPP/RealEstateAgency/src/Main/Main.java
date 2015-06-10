cd2dc9d26dd38bd5180adfe04391bedd2f2a3ff5
sofiene <sofiene.landolsi@gmail.com>
2015-06-10 20:02:04 +0000

    yé hallouuul

--- JAVAAPP/RealEstateAgency/src/Main/Main.java
+++ JAVAAPP/RealEstateAgency/src/Main/Main.java
@@ -22,7 +22,7 @@ public class Main {
 	public static void main(String[] args) {
 
             
-            System.out.println("hello wolrd of fucking java ! ");
+            System.out.println("hello hallouuul ! ");
 	
 	}
     
