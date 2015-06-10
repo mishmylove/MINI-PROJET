1bc96d8f9490adf70eb3462f5dc91dc327856cfe
HALLOUL Mohamed <halloul.mohamed@simulabo.com>
2015-06-10 19:46:35 +0100

    Hello wolrd java

---
+++ JAVAAPP/RealEstateAgency/src/Main/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// main package contains main function ... 
+package Main;
+
+/**
+ *
+ * @author halloul
+ */
+// class main contains the main function
+public class Main {
+    
+    
+    /**
+     * Main function used for test etc ... 
+     * @param args 
+     */
+	public static void main(String[] args) {
+
+            
+            System.out.println("hello wolrd of fucking java ! ");
+	
+	}
+    
+}
