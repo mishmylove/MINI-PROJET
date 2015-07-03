1d554a01e1a2dce72d897ff5824f3dee359f44df
Mejri Mohamed <mohamed.b.mejri@gmail.com>
2015-07-03 18:11:13 +0200

    register

--- WebApp/php/register.php
+++ WebApp/php/register.php
@@ -14,25 +14,20 @@ $sql = 'SELECT * FROM user WHERE mail ="'.$mail.'"' ;
 
 
  if (mysql_query($sql)>=1) {    
-       
+       echo (mysql_query($sql));
         echo "<p>Insertion Failed <br/> Utilisateur existe ...!!</p>";
 }
-
 else {
         $query = mysql_query("INSERT INTO `user`(`id`, `nom`, `prenom`, `mail`, `password`, `adresse`, `telephone`, `type`)
-        VALUES ('','$nom','$prenom','$mail','$pass','$adresse','$tel','$Tcompte')") or die('Erreur SQL !<br />'.$query .'<br />'.mysql_error());
-
+        VALUES ('','$nom','$prenom','$mail','$pass','$adresse','$tel','$Tcompte')") or die('Erreur SQL !<br />'.$cnx .'<br />'.mysql_error());
 if ($query) {
-
 	echo "<br/><br/><span>Data Inserted successfully...!!</span>";
 }
 else{
-
 echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
-}
-
+} 
+  }
 mysql_close($cnx); 
-}
 ?>
 
 
